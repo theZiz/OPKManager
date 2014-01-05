39c12c53f0e80261fa8221562ed581a781046944
Ziz <zizsdl@googlemail.com>
2014-01-05 01:49:11 +0100

    Showing readme

--- make_release_packages.sh
+++ make_release_packages.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Change the folder to YOUR sparrow3d folder!
 PROGRAM="OPKManager"
-VERSION="0.9.10.1"
+VERSION="0.9.10.2"
 DEST=./build/gcw
 echo "<html>" > index.htm
 echo "<head>" >> index.htm
