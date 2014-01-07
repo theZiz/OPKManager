9d2ddfac8888108934ad436e91e63ed84ecb9bcf
Ziz <zizsdl@googlemail.com>
2014-01-07 17:35:23 +0100

    Added opkrun until the firmware supports it itself

--- details.c
+++ details.c
@@ -1,16 +1,16 @@
  /* This file is part of OPKManager.
-  * Sparrow3d is free software: you can redistribute it and/or modify
+  * OPKManager is free software: you can redistribute it and/or modify
   * it under the terms of the GNU General Public License as published by
   * the Free Software Foundation, either version 2 of the License, or
   * (at your option) any later version.
   * 
-  * Sparrow3d is distributed in the hope that it will be useful,
+  * OPKManager is distributed in the hope that it will be useful,
   * but WITHOUT ANY WARRANTY; without even the implied warranty of
   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   * GNU General Public License for more details.
   * 
   * You should have received a copy of the GNU General Public License
-  * along with Foobar.  If not, see <http://www.gnu.org/licenses/>
+  * along with OPKManager.  If not, see <http://www.gnu.org/licenses/>
   * 
   * For feedback and questions about my Files and Projects please mail me,
   * Alexander Matthes (Ziz) , zizsdl_at_googlemail.com */
