3beacb7f0b557a05a3dd4911b47f1aa5c6ab446a
Alexander <ziz@mailbox.org>
2015-10-11 01:56:00 +0200

    Fixed / Added scripts

--- repositories/google_drive.py
+++ repositories/google_drive.py
@@ -7,6 +7,7 @@ import time
 import calendar
 import string
 import sys
+import datetime
 
 class RegisterAction(argparse.Action):
 	def __call__(self, parser, namespace, values, option_string=None):
@@ -18,9 +19,10 @@ class RegisterAction(argparse.Action):
 
 class UpdateAction(argparse.Action):
 	def __call__(self, parser, namespace, values, option_string=None):
-		process = subprocess.Popen('wget --no-check-certificate --timeout='+str(values[0])+' -qO- https://docs.google.com/folderview?id=0B9EgGumkJaabSE9TaEdFbVVBSlU#list',stdout=subprocess.PIPE,shell=True)
+		process = subprocess.Popen('wget --no-check-certificate --timeout='+str(values[0])+' -qO- https://drive.google.com/folderview?hl=en\&id=0B9EgGumkJaabSE9TaEdFbVVBSlU#list',stdout=subprocess.PIPE,shell=True)
 		output = process.stdout.read().split('\n')[1]
 		output = output.split('<div class="flip-entry" id="entry-')
+		now = datetime.datetime.now()
 		first = 1
 		for output_part in output:
 			if first:
@@ -29,20 +31,59 @@ class UpdateAction(argparse.Action):
 			filename = output_part.split('"')[0]
 			real_filename = output_part.split('<div class="flip-entry-title">')[1].split('</div>')[0]
 			splitted_filename = real_filename.split('.opk')
-			version = output_part.split('<div class="flip-entry-last-modified"><div>')[1].split('</div')[0]
-			if len(splitted_filename) < 2:
+			if len(splitted_filename) < 2: # not ending with opk
 				continue
+			version = output_part.split('<div class="flip-entry-last-modified"><div>')[1].split('</div')[0]
 			print "["+splitted_filename[0]+"]"
 			print "download_filename: "+filename
 			print "filename: "+real_filename
-			t = time.strptime(version,"%d.%m.%y")
+			try:
+				t = time.strptime(version,"%m/%d/%y")
+			except ValueError:
+				try:
+					# Mon d format
+					date_split = version.split(' ')
+					day = date_split[1];
+					month = 1
+					if (date_split[0] == 'Jan'):
+						month = 1
+					if (date_split[0] == 'Feb'):
+						month = 2
+					if (date_split[0] == 'Mar'):
+						month = 3
+					if (date_split[0] == 'Apr'):
+						month = 4
+					if (date_split[0] == 'May'):
+						month = 5
+					if (date_split[0] == 'Jun'):
+						month = 6
+					if (date_split[0] == 'Jul'):
+						month = 7
+					if (date_split[0] == 'Aug'):
+						month = 8
+					if (date_split[0] == 'Sep'):
+						month = 9
+					if (date_split[0] == 'Oct'):
+						month = 10
+					if (date_split[0] == 'Nov'):
+						month = 11
+					if (date_split[0] == 'Dec'):
+						month = 12
+					new_version = str(month) + "/" + day + "/" + str(now.year)
+					t = time.strptime(new_version,"%m/%d/%Y")
+				except ValueError:
+					try:
+						new_version = str(now.month) + "/" + str(now.day) + "/" + str(now.year) + " " + version
+						t = time.strptime(new_version,"%m/%d/%Y %I:%M %p")
+					except ValueError:
+						new_version = str(now.month) + "/" + str(now.day) + "/" + str(now.year)
+						t = time.strptime(new_version,"%m/%d/%Y")
 			print "version: " + str(calendar.timegm(t)) #NEEDED!
+			print "description: " + splitted_filename[0]
 			print ""
-		
-		
 
 def main():
-	parser = argparse.ArgumentParser(description="Ziz's Repository script")
+	parser = argparse.ArgumentParser(description="Google Drive Repository script")
 	parser.add_argument('--register', nargs=0, action=RegisterAction)
 	parser.add_argument('--update', nargs=1, action=UpdateAction)
 	args = parser.parse_args()
--- repositories/ziz.py
+++ repositories/ziz.py
@@ -177,7 +177,6 @@ class UpdateAction(argparse.Action):
 		output = process.stdout.read().split('\n')[1]
 		output = output.split('<div class="flip-entry" id="entry-')
 		now = datetime.datetime.now()
-		year = now.year;
 		first = 1
 		for output_part in output:
 			if first:
@@ -186,52 +185,139 @@ class UpdateAction(argparse.Action):
 			filename = output_part.split('"')[0]
 			real_filename = output_part.split('<div class="flip-entry-title">')[1].split('</div>')[0]
 			splitted_filename = real_filename.split('.opk')
-			version = output_part.split('<div class="flip-entry-last-modified"><div>')[1].split('</div')[0]
-			if len(splitted_filename) < 2:
+			if len(splitted_filename) < 2: # not ending with opk
 				continue
+			version = output_part.split('<div class="flip-entry-last-modified"><div>')[1].split('</div')[0]
 			print "["+splitted_filename[0]+"]"
 			print "download_filename: "+filename
 			print "filename: "+real_filename
 			try:
 				t = time.strptime(version,"%m/%d/%y")
 			except ValueError:
-				# Mon d format
-				date_split = version.split(' ')
-				day = date_split[1];
-				month = 1
-				if (date_split[0] == 'Jan'):
+				try:
+					# Mon d format
+					date_split = version.split(' ')
+					day = date_split[1];
 					month = 1
-				if (date_split[0] == 'Feb'):
-					month = 2
-				if (date_split[0] == 'Mar'):
-					month = 3
-				if (date_split[0] == 'Apr'):
-					month = 4
-				if (date_split[0] == 'May'):
-					month = 5
-				if (date_split[0] == 'Jun'):
-					month = 6
-				if (date_split[0] == 'Jul'):
-					month = 7
-				if (date_split[0] == 'Aug'):
-					month = 8
-				if (date_split[0] == 'Sep'):
-					month = 9
-				if (date_split[0] == 'Oct'):
-					month = 10
-				if (date_split[0] == 'Nov'):
-					month = 11
-				if (date_split[0] == 'Dec'):
-					month = 12
-				version = str(month) + "/" + day + "/" + str(year)
-				t = time.strptime(version,"%m/%d/%Y")
+					if (date_split[0] == 'Jan'):
+						month = 1
+					if (date_split[0] == 'Feb'):
+						month = 2
+					if (date_split[0] == 'Mar'):
+						month = 3
+					if (date_split[0] == 'Apr'):
+						month = 4
+					if (date_split[0] == 'May'):
+						month = 5
+					if (date_split[0] == 'Jun'):
+						month = 6
+					if (date_split[0] == 'Jul'):
+						month = 7
+					if (date_split[0] == 'Aug'):
+						month = 8
+					if (date_split[0] == 'Sep'):
+						month = 9
+					if (date_split[0] == 'Oct'):
+						month = 10
+					if (date_split[0] == 'Nov'):
+						month = 11
+					if (date_split[0] == 'Dec'):
+						month = 12
+					new_version = str(month) + "/" + day + "/" + str(now.year)
+					t = time.strptime(new_version,"%m/%d/%Y")
+				except ValueError:
+					try:
+						new_version = str(now.month) + "/" + str(now.day) + "/" + str(now.year) + " " + version
+						t = time.strptime(new_version,"%m/%d/%Y %I:%M %p")
+					except ValueError:
+						new_version = str(now.month) + "/" + str(now.day) + "/" + str(now.year)
+						t = time.strptime(new_version,"%m/%d/%Y")
 			print "version: " + str(calendar.timegm(t)) #NEEDED!
-			print "https://docs.google.com/uc?export=download\&id="
+			print "url_addition: https://www.googledrive.com/host/"
 			print "description: " + splitted_filename[0] + ". Brought to you by David Knight."
-			print ""
+		#Gameblabla
+		#Oswan
+		print "[Oswan]"
+		print "filename: oswan.opk" #NEEDED!
+		print "description: Gameblabla's Oswan is a Wonderswan emulator based on OswanJ and Alekmaul's Oswan. Brought to you by Gameblabla."
+		print "url_addition: https://github.com/gameblabla/oswan/raw/master/"
+		print "image_url: https://gameblabla.olympe.in/img/mmn1.png"
+		process = subprocess.Popen('wget --no-check-certificate --timeout='+str(values[0])+' -qO- https://github.com/gameblabla/oswan',stdout=subprocess.PIPE,shell=True)
+		output = process.stdout.read().split('\n');
+		found = 0
+		for output_part in output:
+			if (output_part.startswith('            <span class="css-truncate css-truncate-target"><a href="/gameblabla/oswan/blob/master/oswan.opk"')):
+				found = 1
+			if (found == 1 and output_part.startswith('            <span class="css-truncate css-truncate-target"><time datetime="')):
+				version = output_part.split('            <span class="css-truncate css-truncate-target"><time datetime="')[1].split('" is="time-ago">')[0]
+				t = time.strptime(version,"%Y-%m-%dT%H:%M:%SZ")
+				print "version: " + str(calendar.timegm(t)) #NEEDED!
+				break
+		print ""
+		#Methane Brothers
+		print "[Super Methane Bros.]"
+		print "filename: methane.opk" #NEEDED!
+		print "description: Super Methane Brothers is a Bubble Bobble clone originally released for Amiga by Apache Software Ltd.. Brought to you by Gameblabla."
+		print "url_addition: https://github.com/gameblabla/methane/raw/master/source/gcw/"
+		print "image_url: https://upload.wikimedia.org/wikipedia/en/4/4b/Super_Methane_Bros_sm.png"
+		t = time.strptime("2015-09-29","%Y-%m-%d")
+		print "version: " + str(calendar.timegm(t)) #NEEDED!
+		print ""
+		#Ganbare! Natsuki-San!
+		print "[Gannatsu Portable]"
+		print "filename: gnp.opk" #NEEDED!
+		print "description: Puzzle game. Brought to you by Gameblabla."
+		print "url_addition: https://gameblabla.olympe.in/files/"
+		print "image_url: http://speeddemosarchive.com/gfx/GanbareNatsukiSan_1.jpg"
+		t = time.strptime("2015-07-28","%Y-%m-%d")
+		print "version: " + str(calendar.timegm(t)) #NEEDED!
+		print ""
+		#Potator
+		print "[Potator]"
+		print "filename: potator.opk" #NEEDED!
+		print "description: Watara Supervision Emulator. Brought to you by Gameblabla."
+		print "url_addition: https://github.com/alekmaul/potator/raw/master/distrib/"
+		t = time.strptime("2015-09-04","%Y-%m-%d")
+		print "version: " + str(calendar.timegm(t)) #NEEDED!
+		print ""
+		#Mednafen
+		process = subprocess.Popen('wget --no-check-certificate --timeout='+str(values[0])+' -qO- https://github.com/gameblabla/mednafen-gcw',stdout=subprocess.PIPE,shell=True)
+		output = process.stdout.read().split('\n');
+		found = 0
+		for output_part in output:
+			if (output_part.startswith('            <span class="css-truncate css-truncate-target"><a href="/gameblabla/mednafen-gcw/tree/master/release"')):
+				found = 1
+			if (found == 1 and output_part.startswith('            <span class="css-truncate css-truncate-target"><time datetime="')):
+				version = output_part.split('            <span class="css-truncate css-truncate-target"><time datetime="')[1].split('" is="time-ago">')[0]
+				t = time.strptime(version,"%Y-%m-%dT%H:%M:%SZ")
+				break
+		print "[Mednafen]"
+		print "filename: mednafen_generic.opk" #NEEDED!
+		print "description: Mednafan for GB/GBA/NES/LYNX. Brought to you by Gameblabla."
+		print "url_addition: https://github.com/gameblabla/mednafen-gcw/raw/master/release/"
+		print "version: " + str(calendar.timegm(t)) #NEEDED!
+		print ""
+		print "[Mednafen PCE]"
+		print "filename: mednafen_pce.opk" #NEEDED!
+		print "description: Mednafan for PCE. Brought to you by Gameblabla."
+		print "url_addition: https://github.com/gameblabla/mednafen-gcw/raw/master/release/"
+		print "version: " + str(calendar.timegm(t)) #NEEDED!
+		print ""
+		print "[Mednafen PCFX]"
+		print "filename: mednafen_pcfx.opk" #NEEDED!
+		print "description: Mednafan for PCFX. Brought to you by Gameblabla."
+		print "url_addition: https://github.com/gameblabla/mednafen-gcw/raw/master/release/"
+		print "version: " + str(calendar.timegm(t)) #NEEDED!
+		print ""
+		print "[Mednafen PSX]"
+		print "filename: mednafen_psx.opk" #NEEDED!
+		print "description: Mednafan for PSX. Brought to you by Gameblabla."
+		print "url_addition: https://github.com/gameblabla/mednafen-gcw/raw/master/release/"
+		print "version: " + str(calendar.timegm(t)) #NEEDED!
+		print ""
 
 def main():
-	parser = argparse.ArgumentParser(description="Ziz's Repository script")
+	parser = argparse.ArgumentParser(description="Community Repository script")
 	parser.add_argument('--register', nargs=0, action=RegisterAction)
 	parser.add_argument('--update', nargs=1, action=UpdateAction)
 	args = parser.parse_args()
